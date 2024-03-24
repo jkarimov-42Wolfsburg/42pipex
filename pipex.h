a379d3ecf33bbac904fda7fb2bca8827bc6c636b
jkarimov <145347964+jkarimov-42Wolfsburg@users.noreply.github.com>
2024-03-24 14:50:49 +0100

    Create pipex.h
    library added

---
+++ pipex.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkarimov <jkarimov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 11:52:28 by jkarimov          #+#    #+#             */
+/*   Updated: 2024/03/12 15:08:05 by jkarimov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+void	ft_error(void);
+
+
+#endif
